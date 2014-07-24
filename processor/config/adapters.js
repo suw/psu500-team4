15b2bf53467f61cec841726a6b1ed204e44ccbad
mac <mac@macs-MacBook-Pro.local>
2014-07-23 21:53:22 -0600

    add mysql adapter

--- processor/config/adapters.js
+++ processor/config/adapters.js
@@ -1,8 +1,8 @@
 /**
  * Global adapter config
- * 
+ *
  * The `adapters` configuration object lets you create different global "saved settings"
- * that you can mix and match in your models.  The `default` option indicates which 
+ * that you can mix and match in your models.  The `default` option indicates which
  * "saved setting" should be used if a model doesn't have an adapter specified.
  *
  * Keep in mind that options you define directly in your model definitions
@@ -22,5 +22,19 @@ module.exports.adapters = {
         user: '',
         password: '',
         database: 'local'
+    },
+
+    mysql: {
+        module   : 'sails-mysql',
+        host     : 'host320.hostmonster.com',
+        port     :  3306,
+        user     : 'fivninni_apikids',
+        password : 'makeitw0rk',
+        database : 'fivninni_sweng500'
+
+    // OR (explicit sets take precedence)
+    //module   : 'sails-mysql',
+    //url      : 'mysql2://USER:PASSWORD@HOST:PORT/DATABASENAME'
     }
+
 };
